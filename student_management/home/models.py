06f46275e2f4f7a058bbf8745dd8269ce1ca0f48
Manav Anreja <anrejamanav@gmail.com>
2025-07-18 12:14:03 +0530

    models created

---
+++ student_management/home/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Class(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Students(models.Model):
+    name = models.CharField(max_length=100)
+    s_class = models.ForeignKey(Class, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    roll_no = models.PositiveIntegerField(unique=True)
